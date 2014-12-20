50fb4b500ca405ab5fe6d4c3ab78528b71e10921
fukuiretu <s0232101@gmail.com>
2014-12-20 15:17:43 +0900

    init

---
+++ lib/ruboty/message_suffix.rb
@@ -0,0 +1,7 @@
+require "ruboty/message_suffix/version"
+
+module Ruboty
+  module MessageSuffix
+    # Your code goes here...
+  end
+end
---
+++ lib/ruboty/message_suffix/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module MessageSuffix
+    VERSION = "0.0.1"
+  end
+end
